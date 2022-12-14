5c40ac4403a83cab3fc748f588c5da56a178c935
KelliBre <Kelli.Brecunier@gmail.com>
2022-12-14 15:33:58 +1000

    commit tree 8a3b6eec7d60434d02063b583a701291a11f8783

---
+++ index.js
@@ -0,0 +1,15 @@
+// Declare using function expression
+const divide = function(){
+    let a = 2000;
+    let b = 100;
+    return a/b
+};
+
+// const square = num1 => {
+//     let x = 2;
+//     return num1 * x
+// }
+
+const square = x => x * 2
+
+const add = (a, b) => a + b
